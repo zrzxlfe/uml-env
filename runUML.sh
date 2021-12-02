b63ab2fa61d7e3106d2789da9cbef487f61cef88
Kevin Zhou <zzl-ko@outlook.com>
2021-12-02 15:15:57 +0800

    更新rootfs, 配置uml与本机互访
    详情参见 runUML.sh ImageChanglog

--- runUML.sh
+++ runUML.sh
@@ -4,7 +4,7 @@
 # Date    : 2020-06-18
 # Version : v1.0
 # Abstract: Manage user mode linux
-# Usages  : 1. UML start: ./runUML.sh [reset | a | u]
+# Usages  : 1. UML start: ./runUML.sh [reset | a | u | S | T]
 #           2. UML stop : via cmd 'halt' or Ctrl+D (Must be root logined)
 #           3. CMD must be run by root: apt git etc.
 # Notes   : Even though run 'git clone' by root, but root don't own these files.
@@ -14,6 +14,9 @@
 ###############################################################################
 ws_path=$(pwd)
 uml_exe=$ws_path/UML-4.19.95
+uml_ip_address='10.1.0.10'
+host_ip_address='10.1.0.1'
+tap_iface_name='tapUML'
 host_share_path="$ws_path/uml-share-with-host" # Must use "" not '' if has $
 
 rootfs_dir_name='rootfs.dir'
@@ -21,11 +24,13 @@ rootfs_img_name='rootfs.img'
 
 memory_cfg_args='rw mem=512M'
 rootfs_cfg_args="ubda=$ws_path/$rootfs_img_name.cow,$ws_path/$rootfs_img_name"
-inet_config_args="eth0=slirp,,$ws_path/slirp-1.0.17"
 init_config_args="init=/uml_init.sh"
+slirp_net_cfg_args="eth0=slirp,,$ws_path/slirp-1.0.17"
+tuntap_net_cfg_args="eth1=tuntap,$tap_iface_name,,"
 
 
 function setup_uml_init_sh() {
+export uml_ip_address host_ip_address
 cat > uml_init.sh << EOF
 #!/bin/sh
 
@@ -37,8 +42,12 @@ mount -t sysfs sys sys/
 
 echo "setup networking..."
 ifconfig lo up
+ifconfig eth1 ${uml_ip_address} netmask 255.255.255.0 up
 ifconfig eth0 10.0.0.10 netmask 255.255.255.0 broadcast 10.0.0.255 up
 route add default gw 10.0.0.1
+
+echo "start ssh service in the background..."
+/etc/init.d/ssh start &
 
 echo "setup host share directory..."
 EOF
@@ -50,7 +59,7 @@ EOF
 echo "mount none /mnt -t hostfs -o $host_share_path" >> uml_init.sh
 echo -e "echo [\`date\`] Enjoy UML! > /mnt/uml_is_ready\n" >> uml_init.sh
 
-echo "su -l uml" >> uml_init.sh
+echo "su -l root" >> uml_init.sh
 if [ "$1" = "a" -o "u" = "$1" ]; then
 	echo "exec /tini-0.19.0 /bin/sh" >> uml_init.sh
 else
@@ -64,6 +73,38 @@ sudo  cp uml_init.sh $rootfs_dir_name
 
 function main() 
 {
+	case $1 in
+		S)  # 只能访问公网，无法访问本机
+			inet_config_args=$slirp_net_cfg_args
+		;;
+		T)  # 只能访问本机，无法访问公网
+			# 注意：！！！ 
+			# 1）此种方式进入uml后即可与主机进行通信（在root用户环境下可以ping通主机，主机也能ping通UML），
+			#    但uml无法访问公网，还需要借助网桥才行(https://blog.csdn.net/qq_34160841/article/details/104901127)
+			# 2）在UML中貌似无法通过Ctrl-C来退出程序，此时只得从主机侧新开一个终端kill掉uml(sudo killall UML-4.19.95)
+			# 3）uml_init.sh 中已配置自动启动 sshd 但启动时间较长，此外启动sshd之后，退出UML时也将比较耗时，只得外部kill
+			# 4）从UML侧无法通过ssh连接主机，但从主机侧可以通过ssh连接UML（连接UML之后却可以再反向ssh主机，暂不知为何）
+			inet_config_args="eth1=tuntap,,,$host_ip_address"
+			uml_exe="sudo $uml_exe"
+		;;
+		*)  # 结合以上 S T 两种方式，配置双网卡，一个只与主机通信，一个用于与公网通信
+			[[ $EUID -ne 0 ]] && echo "This operation must be run as root." && exit 1
+			type tunctl >/dev/null 2>&1
+			if [ $? -ne 0 ]; then
+				echo "uml-utilities not installed. if you want to install?(y/n)"
+				read -t 5 ans
+				[ "$ans" != "y" ] && exit 1
+				sudo apt install uml-utilities
+			fi
+			if [[ `ifconfig $tap_iface_name 2>&1` = *"Device not found"* ]]; then
+				sudo tunctl -t $tap_iface_name
+				sudo ifconfig  $tap_iface_name ${host_ip_address}
+			fi
+			uml_exe="sudo $uml_exe"
+			inet_config_args="$slirp_net_cfg_args $tuntap_net_cfg_args"
+		;;
+	esac
+
 	case $1 in
 		reset) rm -f *.cow *.img ; ls -F -I resources | grep "/$" | xargs rm -rf ; exit 0 ;;
 		a) 
@@ -104,6 +145,10 @@ function main()
 		rootfs_cfg_args="root=/dev/root rootfstype=hostfs rootflags=$ws_path/$rootfs_dir_name"
 	fi
 
+	# 解决UML报错(https://www.cnblogs.com/duanxz/p/3567068.html) 
+	# 'UML ran out of memory on the host side! ... vm.max_map_count has been reached.'
+	sudo sh -c 'sysctl -w vm.max_map_count=524288 && sysctl -a|grep vm.max_map_count'
+
 	echo \
 	$uml_exe $memory_cfg_args $rootfs_cfg_args $inet_config_args $init_config_args
 
@@ -123,4 +168,5 @@ main $@
 # https://www.cnblogs.com/dream397/p/14251536.html
 # https://www.cnblogs.com/dream397/p/14251617.html
 # https://blog.csdn.net/gogofly_lee/article/details/2146602
+# https://jingyan.baidu.com/article/380abd0a0b35501d91192c69.html
 ###############################################################################
